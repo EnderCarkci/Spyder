4b51baccfeb990b572d4b1ebf8ec96eb37a1296e
Ender Çarkçı <123842545+EnderCarkci@users.noreply.github.com>
2023-03-02 17:50:44 +0300

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 17:45:46 2023
+
+@author: Ender
+"""
+print("Hello World!")
+print("MERHABA DÜNYA!")
+#merhaba burada ogrenicegim hersey burada yazılacak
+print("2+2")
+
